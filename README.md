a3c6e20aa549365aed12e8e5d5aaf093d8206f50
joexu01 <joexu01@yahoo.com>
2023-02-06 10:56:04 +0800

    update: README.md

--- README.md
+++ README.md
@@ -1,35 +1,194 @@
-**Update Docs**
+# Container Dispatcher
 
-```shell
-# 1.16 及以上版本
-$ go install github.com/swaggo/swag/cmd/swag@latest
+Container Dispatcher 将 docker 功能再次封装为友好的 WEB 界面，它能够帮助研究者更快速地在容器中验证攻击算法对模型的影响。
 
-$ swag init -g router/router.go
+![](Container-Dispather_01.png)
 
-$ go run main.go
+## 部署需求
+
+* Docker Engine > 19.03
+* MySQL or MariaDB
+* （如果需要编译）go > 1.13
+
+## 编译及部署
+
+1. 克隆仓库
+
+2. 编译（可以跳过，直接使用仓库中已经编译好的二进制文件）
+    ```shell
+    $ go mod tidy
+    $ go build main.go
+    ```
+
+3. 在 MySQL 中创建名为 `container` 的数据库
+4. 执行本仓库中的 `dao/export.sql` 在数据库中创建表和初始化数据
+5. 配置 `conf` 下的数据库连接地址
+6. 以管理员权限运行
+   ```shell
+   $ sudo main
+   ```
+
+## 代码结构
+
+```
+container-dispatcher
+├── certs                     存放证书                        
+│   ├── ca.crt
+│   ├── private.crt
+│   ├── private.csr
+│   ├── private.key
+│   ├── public.key
+│   └── server.pem
+├── conf                      配置文件
+│   ├── dev
+│   └── pm2
+├── controller                API 对应的处理函数
+│   ├── algo_manager.go       算法管理 API
+│   ├── container.go          容器管理 API (Deprecated)
+│   ├── image.go              Docker 镜像管理 API
+│   ├── login.go
+│   ├── resource.go           资源 API
+│   ├── task.go               任务 API
+│   └── user.go               用户管理 API
+├── dao                       Data Access Object 数据访问对象
+│   ├── algo_manage.go
+│   ├── container.go
+│   ├── example.sql
+│   ├── export.sql
+│   ├── task.go
+│   └── user.go
+├── dockerfiles
+│   ├── AIJack.dockerfile
+│   └── start.sh
+├── docs                      Swagger API Docs
+│   ├── docs.go
+│   ├── swagger.json
+│   └── swagger.yaml
+├── dto                       Data Transfer Object 数据传输对象
+│   ├── algo_manager.go
+│   ├── container.go
+│   ├── image.go
+│   ├── resource.go
+│   ├── task.go
+│   └── user.go
+├── go.mod
+├── go.sum
+├── lib
+│   ├── conf.go
+│   ├── context.go
+│   ├── docker.go
+│   ├── file.go
+│   ├── func.go
+│   ├── log.go
+│   ├── mysql.go
+│   └── redis.go
+├── log
+│   ├── config.go
+│   ├── console_writer.go
+│   ├── file_writer.go
+│   ├── log.go
+│   └── log_test.go
+├── logs
+│   ├── golang_common.inf.log
+│   └── golang_common.wf.log
+├── main
+├── main.go
+├── middleware                请求拦截中间件
+│   ├── cors.go
+│   ├── ip_white_list.go
+│   ├── recovery.go
+│   ├── request_log.go
+│   ├── response.go
+│   ├── session_auth.go
+│   ├── translation.go
+│   └── validator-basic.go
+├── public
+│   ├── const.go
+│   ├── log.go
+│   ├── params.go
+│   └── utils.go
+├── README.md
+├── router                    路由
+│   ├── httpserver.go
+│   └── router.go
+├── static           
+│   ├── attack
+│   ├── image
+│   ├── task
+│   └── test.py
+├── test
+│   ├── test.go
+│   └── test_test.go
+├── test.http
+└── tmp
+    └── README
 ```
 
-访问：localhost:8880/swagger/index.html
+### 算法管理
 
+文件位于 `controller/algo_manager.go`。用户可以上传攻击算法供其他用户使用。下面是算法管理使用的 API：
 
+```go
+func AlgorithmControllerRegister(group *gin.RouterGroup) {
+	ac := &AlgorithmController{}
 
----
+	group.POST("/upload", ac.AlgorithmUploadNew)        // 上传算法文件接口
+	group.POST("/new", ac.NewAttackAlgorithm)           // 新建算法接口，调用后修改数据库
+	group.GET("/list", ac.ListAttackAlgorithms)         // 获取算法列表
+	group.GET("/remove/:algo_id", ac.RemoveAlgorithm)   // 软删除算法，参数是算法ID
+}
+```
 
-[Gin Docs](https://gin-gonic.com/docs/)
+### 硬件资源信息
 
-[Gorm Docs](https://gorm.io/zh_CN/docs/)
+`controller/resource.go` 用户可以通过调用接口获取硬件资源信息和当前状态
 
-[bcrypt](https://pkg.go.dev/golang.org/x/crypto/bcrypt)
+```go
+func ResourceControllerRegister(group *gin.RouterGroup) {
+	res := &ResourceController{}
+	group.GET("/gpu", res.ResourceGPUList)              // 获取 GPU 信息
+	group.GET("/cpu", res.ResourceCPUList)              // 获取 CPU 信息
+	group.GET("/gpu/cache/update", res.UpdateGPUSelectionList)      // 更新 GPU 信息缓存
+	group.GET("/gpu/cache", res.GetGPUSelectionList)                // 获取 GPU 信息缓存
+}
+```
 
+### 任务管理
 
----
+`controller/task.go` 任务管理模块负责任务创建和运行
 
-**攻击算法管理**
+```go
+func TaskControllerRegister(group *gin.RouterGroup) {
+	tc := &TaskController{}
+	group.POST("/upload", tc.TaskUploadFiles)               // 上传任务文件接口
+	group.POST("/new", tc.NewTask)                          // 新建任务接口
+	group.GET("/list", tc.ListTasks)                        // 获取任务列表
+	group.GET("/detail/:task_id", tc.TaskDetail)            // 获取任务详情
+	group.GET("/run/:task_id", tc.RunTaskTest)              // 创建容器并执行任务
+	group.GET("/stop/:task_id", tc.StopTask)                // 提前终止任务容器
+	group.GET("/log/:task_id", tc.ShowTaskLog)              // 获取任务容器的日志
+	group.GET("/remove/:task_id", tc.RemoveTask)            // 删除任务文件，数据库中软删除任务信息
 
-攻击算法
+	group.StaticFS("/file/", gin.Dir(lib.GetStringConf("base.task_file.directory"), true))      // 静态文件服务器
+	group.GET("/dir/:task_id", tc.GetTaskFiles)             // 获取任务文件夹下的文件
+	group.GET("/pack", tc.PackAndDownloadFiles)             // 任务文件打包下载
+}
+```
 
-Parameters:
+### 镜像管理
 
-- 用户上传的模型 - 我是不是直接传递个文件路径就好了
+`controller/image.go`
 
-- 数据集 - 这个怎么办：用户自己编写dataloader函数？
+```go
+func ImageControllerRegister(group *gin.RouterGroup) {
+	img := &ImageController{}
+	group.GET("/list", img.ImagesList)                      // 获取镜像列表
+	group.POST("/upload", img.UploadImage)                  // 上传并添加一个镜像文件
+	group.GET("/remove/:image_id", img.RemoveImage)         // 删除镜像
+	group.GET("/pull", img.PullImage)                       // 从 Docker Hub 拉去一个镜像
+}
+```
+
+## 如何添加新的功能
+
+请参考 gin 文档，在 `controller` 文件夹下编写新的 Handler，然后在 `router/router.go` 中注册即可在应用中引入这些新的 API Handler。
--- dao/export.sql
+++ dao/export.sql
@@ -1,77 +1,97 @@
 -- `container-dev`.`role` definition
 
-CREATE TABLE `role` (
-                        `id` smallint(6) NOT NULL,
-                        `desc` varchar(127) NOT NULL DEFAULT '',
-                        `permission_value` int(11) NOT NULL,
-                        PRIMARY KEY (`id`),
-                        UNIQUE KEY `desc` (`desc`),
-                        UNIQUE KEY `permission_value` (`permission_value`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色权限表';
+CREATE TABLE `role`
+(
+    `id`               smallint(6)  NOT NULL,
+    `desc`             varchar(127) NOT NULL DEFAULT '',
+    `permission_value` int(11)      NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `desc` (`desc`),
+    UNIQUE KEY `permission_value` (`permission_value`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4 COMMENT ='角色权限表';
 
 
 -- `container-dev`.`user` definition
 
-CREATE TABLE `user` (
-                        `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
-                        `username` varchar(255) NOT NULL DEFAULT '' COMMENT '用户名',
-                        `hashed_password` varchar(512) NOT NULL DEFAULT '' COMMENT '加盐后密码',
-                        `created_at` datetime NOT NULL DEFAULT '1971-01-01 00:00:00' COMMENT '新增时间',
-                        `updated_at` datetime NOT NULL DEFAULT '1971-01-01 00:00:00' COMMENT '更新时间',
-                        `is_delete` tinyint(4) NOT NULL DEFAULT 0 COMMENT '是否删除',
-                        `email` varchar(255) NOT NULL DEFAULT '' COMMENT '用户邮箱',
-                        `user_role` smallint(6) NOT NULL COMMENT '用户角色',
-                        PRIMARY KEY (`id`),
-                        UNIQUE KEY `username` (`username`),
-                        KEY `role_user_fk` (`user_role`),
-                        CONSTRAINT `role_user_fk` FOREIGN KEY (`user_role`) REFERENCES `role` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COMMENT='用户表';
+CREATE TABLE `user`
+(
+    `id`              bigint(20)   NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+    `username`        varchar(255) NOT NULL DEFAULT '' COMMENT '用户名',
+    `hashed_password` varchar(512) NOT NULL DEFAULT '' COMMENT '加盐后密码',
+    `created_at`      datetime     NOT NULL DEFAULT '1971-01-01 00:00:00' COMMENT '新增时间',
+    `updated_at`      datetime     NOT NULL DEFAULT '1971-01-01 00:00:00' COMMENT '更新时间',
+    `is_delete`       tinyint(4)   NOT NULL DEFAULT 0 COMMENT '是否删除',
+    `email`           varchar(255) NOT NULL DEFAULT '' COMMENT '用户邮箱',
+    `user_role`       smallint(6)  NOT NULL COMMENT '用户角色',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `username` (`username`),
+    KEY `role_user_fk` (`user_role`),
+    CONSTRAINT `role_user_fk` FOREIGN KEY (`user_role`) REFERENCES `role` (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 12
+  DEFAULT CHARSET = utf8mb4 COMMENT ='用户表';
 
 -- `container-dev`.`algorithm` definition
 
-CREATE TABLE `algorithm` (
-                             `path` varchar(1024) NOT NULL COMMENT ' 攻击算法的路径',
-                             `entry_point` varchar(255) NOT NULL,
-                             `author_id` bigint(20) DEFAULT NULL,
-                             `exec_binary` varchar(16) DEFAULT NULL COMMENT 'sh or py2 or py3 or ...',
-                             `name` varchar(100) NOT NULL,
-                             `desc` varchar(512) DEFAULT NULL,
-                             `uuid` varchar(256) NOT NULL,
-                             `files` text NOT NULL COMMENT '文件列表（; seperated）',
-                             `created_at` datetime DEFAULT NULL,
-                             `default_image_name` varchar(256) NOT NULL,
-                             PRIMARY KEY (`uuid`),
-                             KEY `algorithm_FK` (`author_id`),
-                             CONSTRAINT `algorithm_FK` FOREIGN KEY (`author_id`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='攻击算法';
+CREATE TABLE `algorithm`
+(
+    `path`               varchar(1024) NOT NULL COMMENT ' 攻击算法的路径',
+    `entry_point`        varchar(255)  NOT NULL,
+    `author_id`          bigint(20)   DEFAULT NULL,
+    `exec_binary`        varchar(16)  DEFAULT NULL COMMENT 'sh or py2 or py3 or ...',
+    `name`               varchar(100)  NOT NULL,
+    `desc`               varchar(512) DEFAULT NULL,
+    `uuid`               varchar(256)  NOT NULL,
+    `files`              text          NOT NULL COMMENT '文件列表（; seperated）',
+    `created_at`         datetime     DEFAULT NULL,
+    `default_image_name` varchar(256)  NOT NULL,
+    PRIMARY KEY (`uuid`),
+    KEY `algorithm_FK` (`author_id`),
+    CONSTRAINT `algorithm_FK` FOREIGN KEY (`author_id`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4 COMMENT ='攻击算法';
 
 
 -- `container-dev`.container_user definition
 
-CREATE TABLE `container_user` (
-                                  `user_id` bigint(20) NOT NULL,
-                                  `container_id` varchar(255) NOT NULL,
-                                  KEY `container_user_FK` (`user_id`),
-                                  CONSTRAINT `container_user_FK` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户-容器表';
+CREATE TABLE `container_user`
+(
+    `user_id`      bigint(20)   NOT NULL,
+    `container_id` varchar(255) NOT NULL,
+    KEY `container_user_FK` (`user_id`),
+    CONSTRAINT `container_user_FK` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4 COMMENT ='用户-容器表';
 
 
 -- `container-dev`.task definition
 
-CREATE TABLE `task` (
-                        `uuid` varchar(256) NOT NULL,
-                        `task_name` varchar(100) NOT NULL,
-                        `task_desc` varchar(512) DEFAULT NULL,
-                        `algorithm_uuid` varchar(256) NOT NULL,
-                        `user_id` bigint(20) NOT NULL,
-                        `uploaded_files` varchar(100) DEFAULT NULL,
-                        `status` varchar(64) NOT NULL,
-                        `created_at` datetime DEFAULT NULL,
-                        `image_name` varchar(128) NOT NULL,
-                        `container_id` varchar(128) DEFAULT NULL,
-                        PRIMARY KEY (`uuid`),
-                        KEY `task_FK_1` (`user_id`),
-                        KEY `task_FK` (`algorithm_uuid`),
-                        CONSTRAINT `task_FK` FOREIGN KEY (`algorithm_uuid`) REFERENCES `algorithm` (`uuid`),
-                        CONSTRAINT `task_FK_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='机器学习任务';
+CREATE TABLE `task`
+(
+    `uuid`           varchar(256) NOT NULL,
+    `task_name`      varchar(100) NOT NULL,
+    `task_desc`      varchar(512) DEFAULT NULL,
+    `algorithm_uuid` varchar(256) NOT NULL,
+    `user_id`        bigint(20)   NOT NULL,
+    `uploaded_files` varchar(100) DEFAULT NULL,
+    `status`         varchar(64)  NOT NULL,
+    `created_at`     datetime     DEFAULT NULL,
+    `image_name`     varchar(128) NOT NULL,
+    `container_id`   varchar(128) DEFAULT NULL,
+    PRIMARY KEY (`uuid`),
+    KEY `task_FK_1` (`user_id`),
+    KEY `task_FK` (`algorithm_uuid`),
+    CONSTRAINT `task_FK` FOREIGN KEY (`algorithm_uuid`) REFERENCES `algorithm` (`uuid`),
+    CONSTRAINT `task_FK_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4 COMMENT ='机器学习任务';
+
+INSERT INTO `container`.`role` (id, `desc`, permission_value)
+VALUES (1, 'user', 1),
+       (2, 'admin', 2);
+
+INSERT INTO `container`.`user` (username, hashed_password, created_at, updated_at, is_delete, email, user_role)
+VALUES ('joexu01', '$2a$10$2cuYsP3Wlpc59zV3RWan1.jw8oVj/dxuyoJkBOAu4afwm2CL0xjFq', '2022-07-26 15:45:50.0',
+        '2022-07-26 15:45:50.0', 0, 'joexu01@yahoo.com', 2);
+
